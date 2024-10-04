65b650b8342863932e983024a5730238d64406d6
Rajan <rajanj448@student.sfit.ac.in>
2024-10-04 15:25:55 +0530

    text gradient

---
+++ src/app/(landing)/_components/call-to-action/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CallToAction = () => {
+  return (
+    <div className='flex flex-col items-start md:items-center gap-y-5 md:gap-y-0'>
+        
+
+    </div>
+  )
+}
+
+export default CallToAction
---
+++ src/app/(landing)/_components/dashboard-snippet/index.tsx
@@ -0,0 +1,8 @@
+
+const DashboardSnippet = () => {
+  return (
+    <div>DashboardSnippet</div>
+  )
+}
+
+export default DashboardSnippet
