014acb48bce1cda03e2ce115fe2dc5cd3eae6d40
Rajan <rajanj448@student.sfit.ac.in>
2024-10-05 11:04:43 +0530

    Sign In Layout

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,5 +1,6 @@
-"use server";
 
+"use server";
+import { client } from './../lib/prisma';
 import { currentUser } from "@clerk/nextjs/server";
 
 export const onAuthenticatedUser=async()=>{
@@ -7,8 +8,37 @@ export const onAuthenticatedUser=async()=>{
         const clerk=await currentUser();
         if(!clerk) return {status:404};
 
+        const user=await client.user.findUnique({
+            where:{
+                clerkId:clerk.id
+            },
+            select:{
+                id:true,
+                firstname:true,
+                lastname:true
+            }
+        })
+
+        if(user){
+            return{
+                status:200,
+                id:user.id,
+                image:clerk.imageUrl,
+                username:`${user.firstname} ${user.lastname}`
+            }
+
+            
+        }
+
+        return {
+            status:404
+        }
+
         
     } catch (error) {
+        return{
+            status:400
+        }
         
     }
 }
--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <>
+    <h5 className='font-bold text-base text-themeTextWhite'>Login</h5>
+    <p className='text-themeTextGray leading-tight'>
+      Network with people from around the world, join groups, create your own, watch courses and become the best version of yourself.
+    </p>
+    </>
   )
 }
 
