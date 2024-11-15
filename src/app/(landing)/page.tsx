6eaa49022a53c0d1c51cb765d6acc507df28bd24
Rajan <rajanj448@student.sfit.ac.in>
2024-11-15 17:38:55 +0530

    Pricing Part

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,7 +1,16 @@
 import Image from "next/image"
 import CallToAction from "./_components/call-to-action"
 import DashboardSnippet from "./_components/dashboard-snippet"
+import dynamic from "next/dynamic"
 
+
+const PricingSection = dynamic(
+    () =>
+      import("./_components/pricing").then(
+        (component) => component.PricingSection,
+      ),
+    { ssr: true },
+  )
 export default function Home() {
     return (
        <main className="md:px-10 py-20 flex flex-col gap-36">
@@ -10,6 +19,7 @@ export default function Home() {
             <DashboardSnippet />
 
         </div>
+        <PricingSection />
 
        </main>
     )
