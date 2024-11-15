ac788c2eeb9ed9c6fe0ac682f2eb0cd78a4be2e9
Rajan <rajanj448@student.sfit.ac.in>
2024-11-15 17:34:31 +0530

    Dashboard Snippet done

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image"
 import CallToAction from "./_components/call-to-action"
+import DashboardSnippet from "./_components/dashboard-snippet"
 
 export default function Home() {
     return (
        <main className="md:px-10 py-20 flex flex-col gap-36">
         <div>
             <CallToAction />
+            <DashboardSnippet />
 
         </div>
 
