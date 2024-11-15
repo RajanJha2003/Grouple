75d234c50861cd029db84963e78442345418176b
Rajan <rajanj448@student.sfit.ac.in>
2024-11-15 17:28:15 +0530

    Call TO Action

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image"
+import CallToAction from "./_components/call-to-action"
 
 export default function Home() {
     return (
-       <div>
-        
-       </div>
+       <main className="md:px-10 py-20 flex flex-col gap-36">
+        <div>
+            <CallToAction />
+
+        </div>
+
+       </main>
     )
 }
