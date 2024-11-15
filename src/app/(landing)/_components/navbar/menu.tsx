ac4fabd270ded284e476e4e30af6432078b6db06
Rajan <rajanj448@student.sfit.ac.in>
2024-11-15 17:17:20 +0530

    Landing page navbar completed

--- src/app/(landing)/_components/navbar/menu.tsx
+++ src/app/(landing)/_components/navbar/menu.tsx
@@ -3,6 +3,7 @@
 import { Card, CardContent } from '@/components/ui/card';
 import { GROUPLE_CONSTANTS } from '@/constants';
 import { useNavigation } from '@/hooks/navigation'
+import { cn } from '@/lib/utils';
 import Link from 'next/link';
 import React from 'react'
 
@@ -18,17 +19,45 @@ const Menu = ({orientation}:MenuProps) => {
     switch(orientation){
         case 'desktop':
             return(
-                <Card className='bg-themeGray border-themeGray bg-clip-padding '>
-                    <CardContent>
+                <Card className='bg-themeGray border-themeGray bg-clip-padding backdrop--blur__safari backdrop-filter backdrop-blur-2xl bg-opacity-60 p-1 lg:flex hidden rounded-xl  '>
+                    <CardContent className='p-0 flex gap-2'>
                         {
                             GROUPLE_CONSTANTS.landingPageMenu.map((menuItem)=>(
-                                <Link  href={menuItem.path}>
+                                <Link  href={menuItem.path} {...(menuItem.section && {
+                                    onClick:()=>onSetSection(menuItem.path)
+                                })} key={menuItem.id} className={cn("rounded-xl flex gap-2 py-2 px-4 items-center",section==menuItem.path?"bg-[#09090B] border-[#27272A]":"")}>
+                                    {
+                                        section==menuItem.path && menuItem.icon
+                                    } {menuItem.label}
                                 </Link>
                             ))
                         }
                     </CardContent>
                 </Card>
             )
+            case "mobile":
+                return (
+                  <div className="flex flex-col mt-10">
+                    {GROUPLE_CONSTANTS.landingPageMenu.map((menuItem) => (
+                      <Link
+                        href={menuItem.path}
+                        {...(menuItem.section && {
+                          onClick: () => onSetSection(menuItem.path),
+                        })}
+                        className={cn(
+                          "rounded-xl flex gap-2 py-2 px-4 items-center",
+                          section == menuItem.path ? "bg-themeGray border-[#27272A]" : "",
+                        )}
+                        key={menuItem.id}
+                      >
+                        {menuItem.icon}
+                        {menuItem.label}
+                      </Link>
+                    ))}
+                  </div>
+                )
+              default:
+                return <></>
     }
 }
 
