67c172b2a87eb35248ba142d15938bcd26e5199a
Rajan <rajanj448@student.sfit.ac.in>
2024-11-15 16:56:44 +0530

    navigation hooks craeted

---
+++ src/app/(landing)/_components/navbar/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LandingPageNavbar = () => {
+  return (
+    <div className='w-full flex justify-between sticky top-0 items-center py-5 z-50'>
+        <p className='font-bold text-2xl'>Grouple.</p>
+
+    </div>
+  )
+}
+
+export default LandingPageNavbar
---
+++ src/app/(landing)/_components/navbar/menu.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+type MenuProps={
+    orientation:"mobile" | "desktop"
+}
+
+const Menu = ({orientation}:MenuProps) => {
+  
+    return (
+    <div>Menu</div>
+  )
+}
+
+export default Menu
