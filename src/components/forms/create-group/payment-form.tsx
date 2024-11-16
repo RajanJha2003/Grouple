6edc88826e890389f9191ed09a94f4df78bda71f
Rajan <rajanj448@student.sfit.ac.in>
2024-11-16 13:26:16 +0530

    Stripe elements

---
+++ src/components/forms/create-group/payment-form.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {
+    userId: string
+    affiliate: boolean
+    stripeId?: string
+  }
+
+const PaymentForm = ({userId,affiliate,stripeId}:Props) => {
+  return (
+    <div>PaymentForm</div>
+  )
+}
+
+export default PaymentForm
---
+++ src/hooks/payment/index.ts
@@ -0,0 +1,8 @@
+import { StripeCardElement, loadStripe } from "@stripe/stripe-js"
+   
+export const useStripeElements = () => {
+    const StripePromise = async () =>
+      await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISH_KEY as string)
+  
+    return { StripePromise }
+  }
