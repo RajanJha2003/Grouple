1719b14897b243e3a69883af381b8fadd0b1a17f
Rajan <rajanj448@student.sfit.ac.in>
2024-11-17 11:00:58 +0530

    Stripe connect done

--- src/components/global/group-list-slider/index.tsx
+++ src/components/global/group-list-slider/index.tsx
@@ -4,6 +4,10 @@ import {UseFormRegister} from 'react-hook-form'
 import {SwiperProps,SwiperSlide} from 'swiper/react'
 import Slider from '../slider'
 import { GROUPLE_CONSTANTS } from '@/constants'
+import Link from 'next/link'
+import { GroupListItem } from './list-item'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
 
 
 
@@ -18,15 +22,43 @@ type Props={
 
 const GroupListSlider = ({overlay,label,register,selected,route,...rest}:Props) => {
   return (
-    <Slider slidesPerView={"auto"} spaceBetween={10} loop freeMode label={label} overlay={overlay} {...rest}>
-        {
-            GROUPLE_CONSTANTS.groupList.map((item,i)=>(
-                <SwiperSlide>
-                    
-                </SwiperSlide>
-            ))
-        }
-
+    <Slider
+      slidesPerView={"auto"}
+      spaceBetween={10}
+      loop
+      freeMode
+      label={label}
+      overlay={overlay}
+      {...rest}
+    >
+      {GROUPLE_CONSTANTS.groupList.map((item, i) => (
+        <SwiperSlide key={item.id} className="content-width-slide ">
+          {!register ? (
+            route ? (
+              <Link href={`/explore/${item.path}`}>
+                <GroupListItem {...item} selected={selected} />
+              </Link>
+            ) : (
+              <GroupListItem {...item} />
+            )
+          ) : (
+            i > 0 && (
+              <Label htmlFor={`item-${item.id}`}>
+                <span>
+                  <Input
+                    id={`item-${item.id}`}
+                    type="radio"
+                    className="hidden"
+                    value={item.path}
+                    {...register("category")}
+                  />
+                  <GroupListItem {...item} selected={selected} />
+                </span>
+              </Label>
+            )
+          )}
+        </SwiperSlide>
+      ))}
     </Slider>
   )
 }
